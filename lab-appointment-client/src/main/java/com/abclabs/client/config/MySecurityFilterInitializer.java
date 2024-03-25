b87cc6ee2e71adee1594c681d4fdbf568af4cad8
BhanukaManoramya <69533062+BhanukaManoramya@users.noreply.github.com>
2024-03-25 14:01:45 +0530

    Delete lab-appointment-client/src/main directory

--- lab-appointment-client/src/main/java/com/abclabs/client/config/MySecurityFilterInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.abclabs.client.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class MySecurityFilterInitializer extends AbstractSecurityWebApplicationInitializer{
-
-}
